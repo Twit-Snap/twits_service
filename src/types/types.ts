d27fc2b527adb449893e190a1812f6d6cf4d6e4f
Luis Escalante <lescalante@fi.uba.ar>
2024-09-14 12:51:37 -0300

    Add poc of tweet schema

--- src/types/types.ts
+++ src/types/types.ts
@@ -6,3 +6,51 @@ export type SnapResponse = {
 export type CreateSnapBody = {
   message: string;
 };
+
+
+
+// poc model
+
+
+export interface Tweet {
+  _id: string;
+  created_at: string;
+  entities: Entities;
+  favorite_count: number;
+  in_reply_to_status_id: string | null;
+  in_reply_to_user_id: string | null;
+  lang: string;
+  retweet_count: number;
+  text: string;
+  user: UserTweet;
+  user_id: number;
+}
+
+export interface Entities {
+  hashtags: Hashtag[];
+  user_mentions: UserMention[];
+  urls: URL[];
+}
+
+export interface Hashtag {
+  text: string;
+  indices: number[];
+}
+
+export interface URL {
+  url: string;
+  indices: number[];
+}
+
+export interface UserMention {
+  id: number;
+  username: string;
+  indices: number[];
+}
+
+export interface UserTweet {
+  id: number;
+  name: string;
+  username: string;
+  verified: boolean;
+}
