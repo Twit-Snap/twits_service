6c8e70cb767112c2528ffc07c5d827e90e3da246
Maximiliano Otero Silvera <maximilianotero7@gmail.com>
2024-10-06 21:22:58 -0300

    tests: integration tests for /snaps/by_users query params

--- src/__tests__/snapUsers.test.ts
+++ src/__tests__/snapUsers.test.ts
@@ -108,6 +108,193 @@ describe('Snap users related API Tests', () => {
       const response = await request(app).post('/snaps/by_users');
 
       expect(response.status).toBe(400);
+      expect(response.body).toEqual({
+        'custom-field': 'usersId',
+        detail: 'Users IDs required!',
+        instance: '/snaps/by_users',
+        status: 400,
+        title: 'Validation Error',
+        type: 'about:blank'
+      });
+    });
+
+    it('should return a status code of 400 when usersIds is not a list', async () => {
+      const response = await request(app)
+        .post('/snaps/by_users')
+        .send({ usersIds: { ids: [1, 2, 3] } });
+
+      expect(response.status).toBe(400);
+      expect(response.body).toEqual({
+        'custom-field': 'usersId',
+        detail: 'Users IDs must be an array of IDs!',
+        instance: '/snaps/by_users',
+        status: 400,
+        title: 'Validation Error',
+        type: 'about:blank'
+      });
+    });
+
+    it('should return up to limit items if limit is specified', async () => {
+      await TwitSnap.create({
+        user: {
+          userId: 1,
+          name: 'Test User 1',
+          username: 'testuser1'
+        },
+        content: 'Test snap 1'
+      });
+      await TwitSnap.create({
+        user: {
+          userId: 2,
+          name: 'Test User 2',
+          username: 'testuser2'
+        },
+        content: 'Test snap 2'
+      });
+      await TwitSnap.create({
+        user: {
+          userId: 3,
+          name: 'Test User 3',
+          username: 'testuser3'
+        },
+        content: 'Test snap 3'
+      });
+
+      const response = await request(app)
+        .post('/snaps/by_users')
+        .send({ usersIds: [1, 2, 3] })
+        .query({ limit: 2 })
+        .expect(200);
+
+      expect(response.body.data).toHaveLength(2);
+    });
+
+    it('should return up to 20 items if limit is not specified', async () => {
+      for (let i = 0; i < 25; i++) {
+        await TwitSnap.create({
+          user: {
+            userId: 1,
+            name: `Test User 1`,
+            username: `testuser1`
+          },
+          content: `Test snap 1`
+        });
+      }
+
+      const response = await request(app)
+        .post('/snaps/by_users')
+        .send({ usersIds: [1] })
+        .expect(200);
+
+      expect(response.body.data).toHaveLength(20);
+    });
+
+    it('should return items older than createdAt as default behaviour', async () => {
+      await TwitSnap.create({
+        user: {
+          userId: 1,
+          name: 'Test User 1',
+          username: 'testuser1'
+        },
+        content: 'Test snap 1'
+      });
+      const twit = await TwitSnap.create({
+        user: {
+          userId: 2,
+          name: 'Test User 2',
+          username: 'testuser2'
+        },
+        content: 'Test snap 2'
+      });
+      await TwitSnap.create({
+        user: {
+          userId: 3,
+          name: 'Test User 3',
+          username: 'testuser3'
+        },
+        content: 'Test snap 3'
+      });
+
+      const response = await request(app)
+        .post('/snaps/by_users')
+        .send({ usersIds: [1, 2, 3] })
+        .query({ createdAt: twit.createdAt })
+        .expect(200);
+
+      expect(response.body.data).toHaveLength(1);
+      expect(response.body.data.map((snap: SnapResponse) => snap.content)).toEqual(['Test snap 1']);
+    });
+
+    it('should return items older than createdAt if older is false', async () => {
+      await TwitSnap.create({
+        user: {
+          userId: 1,
+          name: 'Test User 1',
+          username: 'testuser1'
+        },
+        content: 'Test snap 1'
+      });
+      const twit = await TwitSnap.create({
+        user: {
+          userId: 2,
+          name: 'Test User 2',
+          username: 'testuser2'
+        },
+        content: 'Test snap 2'
+      });
+      await TwitSnap.create({
+        user: {
+          userId: 3,
+          name: 'Test User 3',
+          username: 'testuser3'
+        },
+        content: 'Test snap 3'
+      });
+
+      const response = await request(app)
+        .post('/snaps/by_users')
+        .send({ usersIds: [1, 2, 3] })
+        .query({ createdAt: twit.createdAt, older: false })
+        .expect(200);
+
+      expect(response.body.data).toHaveLength(1);
+      expect(response.body.data.map((snap: SnapResponse) => snap.content)).toEqual(['Test snap 1']);
+    });
+
+    it('should return items newer than createdAt if older is true', async () => {
+      await TwitSnap.create({
+        user: {
+          userId: 1,
+          name: 'Test User 1',
+          username: 'testuser1'
+        },
+        content: 'Test snap 1'
+      });
+      const twit = await TwitSnap.create({
+        user: {
+          userId: 2,
+          name: 'Test User 2',
+          username: 'testuser2'
+        },
+        content: 'Test snap 2'
+      });
+      await TwitSnap.create({
+        user: {
+          userId: 3,
+          name: 'Test User 3',
+          username: 'testuser3'
+        },
+        content: 'Test snap 3'
+      });
+
+      const response = await request(app)
+        .post('/snaps/by_users')
+        .send({ usersIds: [1, 2, 3] })
+        .query({ createdAt: twit.createdAt, older: true })
+        .expect(200);
+
+      expect(response.body.data).toHaveLength(1);
+      expect(response.body.data.map((snap: SnapResponse) => snap.content)).toEqual(['Test snap 3']);
     });
   });
 });
