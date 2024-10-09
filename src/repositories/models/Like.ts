7376823e237c39ddf350a365b3e4bcb60ef460c3
Maximiliano Otero Silvera <maximilianotero7@gmail.com>
2024-10-09 20:58:28 -0300

    feat: Like model & schema

---
+++ src/repositories/models/Like.ts
@@ -0,0 +1,15 @@
+import mongoose, { Document, Schema } from 'mongoose';
+
+export interface ILikeModel extends Document {
+  userId: Number;
+  twitId: string;
+  createdAt: string;
+}
+
+const likeSchema = new Schema({
+  userId: { type: Number, required: true },
+  twitId: { type: String, ref: 'TwitSnap', required: true },
+  createdAt: { type: Date, default: Date.now }
+});
+
+export default mongoose.model<ILikeModel>('Like', likeSchema);
