8dca7545cc7fe0c0bf5737645357a1d0392ac6c9
Maximiliano Otero Silvera <maximilianotero7@gmail.com>
2024-10-09 20:59:07 -0300

    feat: like controller

---
+++ src/controllers/likeController.ts
@@ -0,0 +1,61 @@
+import { NextFunction, Request, Response } from 'express';
+import { LikeRepository } from '../repositories/likeRepository';
+import { LikeService } from '../service/likeService';
+
+const likeService = new LikeService();
+const likeRepository = new LikeRepository();
+
+export const getLikesByTwit = async (
+  req: Request<{ twitId: string }>,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    likeService.validateTwitId(req.params.twitId);
+
+    const { twitId } = req.params;
+
+    const data = await likeRepository.getLikesByTwit(twitId);
+
+    res.status(200).json({ data: data });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const addLike = async (
+  req: Request<{ userId: number; twitId: string }>,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    likeService.validateTwitId(req.body.twitId);
+    likeService.validateUserId(req.body.userId);
+
+    const { userId, twitId } = req.body;
+
+    const data = await likeRepository.add(userId, twitId);
+
+    res.status(201).json({ data: data });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const removeLike = async (
+  req: Request<{ userId: number; twitId: string }>,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    likeService.validateTwitId(req.body.twitId);
+    likeService.validateUserId(req.body.userId);
+
+    const { userId, twitId } = req.body;
+
+    await likeRepository.remove(userId, twitId);
+    res.status(204).send();
+  } catch (error) {
+    next(error);
+  }
+};
