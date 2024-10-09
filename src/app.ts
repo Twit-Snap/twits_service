122c6c8ea245dfe236b88c012ddc7207c4744354
Maximiliano Otero Silvera <maximilianotero7@gmail.com>
2024-10-09 20:59:36 -0300

    feat: use likes router

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
+import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import snapRoutes from './routes/snapRoutes';
-import hashtagsRoutes from './routes/hashtagsRoutes';
 import { errorHandler } from './middleware/errorHandler';
+import hashtagsRoutes from './routes/hashtagsRoutes';
+import likeRoutes from './routes/likesRoutes';
+import snapRoutes from './routes/snapRoutes';
 import logger from './utils/logger';
 
 dotenv.config();
@@ -17,6 +18,7 @@ app.use(express.json());
 // Routes
 app.use('/snaps', snapRoutes);
 app.use('/hashtags', hashtagsRoutes);
+app.use('/likes', likeRoutes);
 
 // Error handling middleware
 app.use(errorHandler);
