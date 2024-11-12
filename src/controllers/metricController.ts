826e198b12c41046619d8f7b1a0ea4af6f33586b
Tinchocw <mcwikla@fi.uba.ar>
2024-11-12 13:45:04 -0300

    feat: New metricController

---
+++ src/controllers/metricController.ts
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+
+export class MetricController{
+
+  constructor() {}
+
+  private async createMetric(username: string, type: string, errorMessage: string) {
+    await axios
+      .post(`${process.env.METRIC_SERVICE_URl}/metrics`, {
+        type: type,
+        createdAt: new Date(),
+        username: username,
+        metrics: {}
+      }).catch((error) => {
+        console.error(errorMessage, error);
+    })
+  }
+
+  async createTwitMetric(username: string) {
+    await this.createMetric(username, 'twit', 'Error posting twit metric');
+  }
+  async createLikeMetric(username: string) {
+    await this.createMetric(username, 'like', 'Error posting like metric');
+  }
+  async createRetwitMetric(username: string) {
+    await this.createMetric(username, 'retwit', 'Error posting retwit metric');
+  }
+  async createCommentMetric(username: string) {
+    await this.createMetric(username, 'Comment', 'Error posting comment metric');
+  }
+
+}
