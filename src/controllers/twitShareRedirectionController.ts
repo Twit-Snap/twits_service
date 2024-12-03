3becaaa711d385475808bd50dfafcc74c6b5dd76
Maximiliano Otero Silvera <maximilianotero7@gmail.com>
2024-12-02 23:25:57 -0300

    fix: redirect

--- src/controllers/twitShareRedirectionController.ts
+++ src/controllers/twitShareRedirectionController.ts
@@ -5,7 +5,7 @@ export const redirectToTwit = (req: Request, res: Response) => {
 
   try {
     console.log(`Redirecting to twit with id: ${twitId}`);
-    res.redirect(`myapp://twits/${twitId}`);
+    res.redirect(`twitsnap://twits/${twitId}`);
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
   } catch (error) {
     res.status(500).send({ message: 'Internal server error' });
